3fa0ea9040833bbb034fbc39b58145b845e3b870
DMSys <open.dmsys@gmail.com>
2015-07-10 20:21:14 +0300

    init

---
+++ NStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NStore.Startup))]
+namespace NStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
