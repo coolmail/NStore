bb09d4b21aa4e26197e627e4222678c883fb7bdd
DMSys <open.dmsys@gmail.com>
2015-07-17 09:48:28 +0300

    Depot

---
+++ Depot/Models/AccountModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Depot.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.ErrorMessages))]
+        [Display(Name = "Потребител / E-Mail")]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.ErrorMessages))]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомни ме")]
+        public bool RememberMe { get; set; }
+    }
+}
