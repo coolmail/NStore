592af42f7196780959903ce69347ce45ce6e504a
DMSys <open.dmsys@gmail.com>
2015-07-18 22:52:09 +0300

    Depot

---
+++ Depot/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Depot.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Account/Login
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            // System.Web.Security.FormsAuthentication.SignOut();
+            /*
+            string cultureCode = "bg";
+
+            //Default Language/Culture for all number, Date format  
+            System.Threading.Thread.CurrentThread.CurrentCulture =
+            System.Globalization.CultureInfo.CreateSpecificCulture("en");
+
+            //Ui Culture for Localized text in the UI  
+            System.Threading.Thread.CurrentThread.CurrentUICulture =
+            new System.Globalization.CultureInfo(cultureCode);   
+            */
+            return View();
+        }
+
+        //
+        // GET: /Account/LogOff
+        [AllowAnonymous]
+        public ActionResult LogOff()
+        {
+            // Затрива сесията
+            Session.RemoveAll();
+            // Затрива Cookie на IIS
+            FormsAuthentication.SignOut();
+
+            return Redirect("~/");
+        }
+    }
+}
