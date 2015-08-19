4d61cfce6b92fd80c6fc196ac8a499559353872c
DMSys <open.dmsys@gmail.com>
2015-08-19 23:46:57 +0300

    contractor

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,3 +1,4 @@
 ﻿/// <reference path="jQuery/jquery-2.1.4.min.js" />
 /// <reference path="jQuery/ui-1.11.4/jquery-ui.min.js" />
 /// <reference path="Bootstrap/js/bootstrap.min.js" />
+/// <reference path="script.js" />
---
+++ Scripts/script.js
@@ -0,0 +1,46 @@
+﻿
+function dmDatepicker(htmlItem) {
+    $(htmlItem).datepicker({
+        dateFormat: "dd.mm.yy"
+    });
+}
+
+function dmAutocomplete(sourceUrl, itemName, itemId) {
+    $(itemName).autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: sourceUrl,
+                data: "{'term':'" + request.term + "'}",
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return item;
+                    }))
+                },
+                error: function (response) {
+                    alert(response.responseText);
+                },
+                failure: function (response) {
+                    alert(response.responseText);
+                }
+            });
+        },
+        select: function (event, ui) {
+        },
+        change: function (event, ui) {
+            console.log(ui.item);
+            if (ui.item == null) {
+                $(itemId).val('');
+            }
+            else {
+                $(itemId).val(ui.item.id);
+            }
+        },
+        messages: {
+            noResults: '',
+            results: function () { }
+        }
+    });
+}
